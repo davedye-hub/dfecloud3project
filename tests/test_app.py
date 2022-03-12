54928257df237ebfecae20f0df7f8e6e3a954126
Ubuntu <davedye@development-server.sbidi0dedydetjtn3nwuabfo4d.zx.internal.cloudapp.net>
2022-03-12 14:33:19 +0000

    DD: Create items feature

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,13 @@
+from flask import url_for
 from flask_testing import TestCase
-from application import app, db
+from application import app, db, routes
 from application.models import Items
 
 class TestBase(TestCase):
 
     def create_app(self):
         # Defines the flask object's configuration for the unit tests
-        app.config.update(
-            DATABASE_URI='sqlite:///',
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
             DEBUG=True,
             WTF_CSRF_ENABLED=False
         )
@@ -15,8 +15,9 @@ class TestBase(TestCase):
 
     def setUp(self):
         # Will be called before every test
+        db.drop_all()
         db.create_all()
-        item = Items(item="new item")
+        item = Items(description="new item")
         db.session.add(item)
         db.session.commit()
 
